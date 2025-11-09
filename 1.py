753dbb1f30aa880d1724186160cf5b43e5010c71
lxckysky <sasawat1904@gmail.com>
2025-11-09 14:43:28 +0700

    fucknigga

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-print("I Love KMITL")
+nums = input("Input: ")
+
+nums = nums.strip("[]")           
+numbers = [int(x) for x in nums.split(",")]
+
+total = sum(n for n in numbers if n % 2 == 0)
+
+print("Output:", total)
